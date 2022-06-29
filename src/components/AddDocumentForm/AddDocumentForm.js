9b53a36ac3ae0709a83e4fa4f29758a0655a35a7
Nicole Cevey <nicole.cevey@gmail.com>
2022-06-29 11:31:05 -0400

    Adding functionality to post new document to firebase

--- src/components/AddDocumentForm/AddDocumentForm.js
+++ src/components/AddDocumentForm/AddDocumentForm.js
@@ -1,104 +1,111 @@
 import "./AddDocumentForm.scss";
 import documentIcon from "../../assets/document-512.png";
 import { Link } from "react-router-dom";
-import { Component } from "react";
-import axios from "axios";
+import { addDoc, collection, getFirestore}  from "firebase/firestore";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-class AddDocumentForm extends Component {
+function AddDocumentForm() {
+  let history = useHistory()
 
-    state = {
-        title: "",
-        isOpen: true,
-        version: 0,
-        forReview: true,
-        reviewerName: "",
-    }
-
-  changeHandler = (event) => {
-    this.setState({
-        [event.target.name]: event.target.value,
-    });
-  };
-
-  submitHandler = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    let newDocument = {
-        title: this.state.title,
-        isOpen: this.state.isOpen,
-        version: this.state.version,
-        forReview: this.state.forReview,
-        reviewerName: this.state.reviewerName,
-    }
-    axios.post("http://localhost:8080/documents/add", newDocument).then(() => {
-        const { history: { push } } = this.props;
-        push("/documents")
+    const db = getFirestore();
+    const documentRef = collection(db, "documents")
+
+    addDoc(documentRef, {
+      title: event.target.title.value,
+      status: event.target.status.value,
+      version: event.target.version.value,
+      toReview: event.target.toReview.value,
+      reviewerName: event.target.reviewerName.value,
+      version: event.target.version.value,
+      dateLastReviewed: event.target.dateLastReviewed.value
     })
+    history.push("/documents")
   };
 
-  render() {
-    return (
-      <section className="add-form">
-        <Link to="/documents" className="add-form__link">
-          <button className="add-form__back-button">Back</button>
-        </Link>
-        <h1 className="add-form__title">Add Document</h1>
-        <div className="add-form__container">
-          <img src={documentIcon} className="add-form__icon"></img>
-          <form className="add-form__form">
-            <label className="add-form__label add-form__label--title">
-              Document Title:
-              <input type="text" className="add-form__input"></input>
-            </label>
-            <p className="add-form__label">Document Status:</p>
-            <label>
-              <input
-                type="radio"
-                name="status"
-                value="open"
-                className="add-form__radio"
-              ></input>{" "}
-              Open
-              <input
-                type="radio"
-                name="status"
-                value="closed"
-                className="add-form__radio"
-              ></input>{" "}
-              Closed
-            </label>
-            <label className="add-form__label">
-              Version Number:
-              <input className="add-form__input" type="number"></input>
-            </label>
-            <p className="add-form__label">Waiting for your review:</p>
-            <label className="add-form__radio-label">
-              <input
-                type="radio"
-                name="review"
-                value="yes"
-                className="add-form__radio"
-              ></input>{" "}
-              Yes
-              <input
-                type="radio"
-                name="review"
-                value="no"
-                className="add-form__radio"
-              ></input>{" "}
-              No
-            </label>
-            <label className="add-form__label">
-              Reviewer Name:
-              <input type="text" className="add-form__input"></input>
-            </label>
-            <button type="submit" className="add-form__add-button">
-              Add
-            </button>
-          </form>
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section className="add-form">
+      <Link to="/documents" className="add-form__link">
+        <button className="add-form__back-button">Back</button>
+      </Link>
+      <h1 className="add-form__title">Add Document</h1>
+      <div className="add-form__container">
+        <img src={documentIcon} className="add-form__icon"></img>
+        <form className="add-form__form" onSubmit={submitHandler}>
+          <label className="add-form__label add-form__label--title">
+            Document Title:
+            <input 
+              type="text" 
+              className="add-form__input" 
+              name="title"
+            ></input>
+          </label>
+          <p className="add-form__label">Document Status:</p>
+          <label>
+            <input
+              type="radio"
+              name="status"
+              value="open"
+              className="add-form__radio"
+            ></input>{" "}
+            Open
+            <input
+              type="radio"
+              name="status"
+              value="closed"
+              className="add-form__radio"
+            ></input>{" "}
+            Closed
+          </label>
+          <label className="add-form__label">
+            Version Number:
+            <input 
+              className="add-form__input" 
+              type="number" 
+              name="version"
+            ></input>
+          </label>
+          <p className="add-form__label">Waiting for your review:</p>
+          <label className="add-form__radio-label">
+            <input
+              type="radio"
+              name="toReview"
+              value="yes"
+              className="add-form__radio"
+            ></input>{" "}
+            Yes
+            <input
+              type="radio"
+              name="toReview"
+              value="no"
+              className="add-form__radio"
+            ></input>{" "}
+            No
+          </label>
+          <label className="add-form__label">
+            Reviewer Name:
+            <input 
+              type="text" 
+              className="add-form__input"
+              name="reviewerName"
+            ></input>
+          </label>
+          <label className="add-form__label"> Date Last Reviewed
+            <input 
+              type="date" 
+              name="dateLastReviewed"
+              className="add-form__input"
+            ></input>
+          </label>
+          <button type="submit" className="add-form__add-button">
+            Add
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default AddDocumentForm;
--- src/pages/AddDocumentPage/AddDocumentPage.js
+++ src/pages/AddDocumentPage/AddDocumentPage.js
@@ -1,6 +1,5 @@
 import "./AddDocumentPage.scss";
 import AddDocumentForm from "../../components/AddDocumentForm/AddDocumentForm";
-import { Link } from "react-router-dom";
 
 function AddDocumentPage() {
     return (
