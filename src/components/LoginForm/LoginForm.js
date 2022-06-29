d46cb77d91b0d9e2d980160662a284a17e8824aa
Nicole Cevey <nicole.cevey@gmail.com>
2022-06-28 22:26:25 -0400

    Fixing bug with login and signup modal

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,14 +2,14 @@ import "./LoginForm.scss";
 import React, { useState } from "react";
 import { useAuth } from "../../Contexts/AuthContext"
 import SignupForm from "../SignupForm/SignupForm";
-
+import { useHistory } from "react-router-dom";
 
 function LoginForm() {
   const {login} = useAuth()
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [error, setError] = useState('')
   const [ loading, setLoading ] = useState(false)
-  // const history = useHistory()
+  const history = useHistory()
 
    async function handleLogin(e) {
     e.preventDefault();
@@ -22,6 +22,7 @@ function LoginForm() {
       setError("")
       setLoading(true)
       await login(e.target.email.value, e.target.password.value)
+      history.push("/documents")
     } catch {
       setError("Failed to sign in.")
     }
@@ -35,7 +36,7 @@ function LoginForm() {
       <>
         <form onSubmit={handleLogin} className="login-form">
           <h1 className="login-form__title">Login</h1>
-          <input type="text" className="login-form__input" placeholder="Username" name="username"></input>
+          <input type="text" className="login-form__input" placeholder="Email" name="email"></input>
           <input type="text" className="login-form__input" placeholder="Password" name="password"></input>
           <button type="submit" className="login-form__button">Login</button>
           <div className="login-form__signup">
