a4c764577c08a8cfc62c410570127e2123f674eb
Nicole Cevey <nicole.cevey@gmail.com>
2022-07-02 19:45:23 -0400

    Styling changes to hero on welcome page

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -1,17 +1,9 @@
 import "./HomeHero.scss";
-import documentIcon from "../../assets/document-512-white.png";
 
 function HomeHero() {
     return (
         <main className="hero">
-            <div className="hero__row">
-                <img 
-                    src={documentIcon}
-                    className="hero__icon"
-                    alt="Document icon"
-                ></img>
-                <p className="hero__desc"><span className="hero__desc--bold">DocTurn</span> is a version control management application for busy business professionals.</p>
-            </div>
+            <p className="hero__tagline">Version control management made easy.</p>
         </main>
     )
 }
